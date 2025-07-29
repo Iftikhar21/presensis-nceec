d5da2283552bec1364a0a1263fa6c58b4f1e113f
TUF GAMING F15 <iftikharazharchaudhry@gmail.com>
2025-07-29 08:18:20 +0700

    ubah field date

--- pages/teacher/list-materi/material-list.php
+++ pages/teacher/list-materi/material-list.php
@@ -441,7 +441,7 @@ $title_page = "NCEEC";
 
                     <div class="mb-6">
                         <label class="block text-sm font-medium text-gray-700 mb-2">Waktu Pembelajaran *</label>
-                        <input type="datetime-local" id="waktu-pembelajaran" name="waktu" class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500">
+                        <input type="datetime-local" id="waktu-pembelajaran" step="1" name="waktu" class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500">
                     </div>
 
                     <div class="flex justify-end space-x-4">
